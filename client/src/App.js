c4282791e38c865d0b3f2370b4cca7cc51a9fa5d
vivekbheke <vivek.bheke@stuff.co.nz>
2023-08-08 23:20:32 +1200

    404 page sucks

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter as Router ,Switch, Route} from 'react-router-dom'
 
 import NZNews from './pages/NZNews'
 import AUNews from './pages/AUNews'
@@ -9,13 +9,15 @@ import NotFoundPage from './pages/404'
 const App = () => {
 	return (
 		<div className='maindiv'>
-			<BrowserRouter>
-				<Route path="/" exact component={NZNews} />
-				<Route path="/newzealand-news" exact component={NZNews} />
-				<Route path="/australia-uk-news" exact component={AUNews} />
-				<Route path="/world-news" exact component={WorldNews} />
-				<Route path="*" component={NotFoundPage} />
-			</BrowserRouter>
+			<Router>
+				<Switch>
+					<Route path="/" exact component={NZNews} />
+					<Route path="/newzealand-news" exact component={NZNews} />
+					<Route path="/australia-uk-news" exact component={AUNews} />
+					<Route path="/world-news" exact component={WorldNews} />
+					<Route component={NotFoundPage} />
+				</Switch>
+			</Router>
 		</div>
 	)
 }
