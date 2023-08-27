b310163277d09d14c824dbb3feb1c91ad0f4ffd1
vivekbheke <vivek.bheke@stuff.co.nz>
2023-08-27 15:46:18 +1200

    scroll plus scroll progress bar

--- client/src/pages/GoToTop.js
+++ client/src/pages/GoToTop.js
@@ -1,17 +1,44 @@
 import React from 'react';
 
 const ScrollToTopButton = () => {
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth', // Smooth scrolling animation
+  // const scrollToTop = () => {
+  //   window.scrollTo({
+  //     top: 0,
+  //     behavior: 'smooth', // Smooth scrolling animation
+  //   });
+  // };
+
+  let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 0) {
+      scrollProgress.style.display = "grid";
+    } else {
+      scrollProgress.style.display = "grid";
+    }
+    scrollProgress.addEventListener("click", () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
     });
+    scrollProgress.style.background = `conic-gradient(#009dff ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
   };
 
-  return (
-    <button className="scroll-to-top-button" onClick={scrollToTop}>
+  window.onscroll = calcScrollValue;
+  window.onload = calcScrollValue;
 
-    </button>
+  return (
+    <div id="progress">
+      <span id="progress-value"></span>
+    </div>
+    // <button className="scroll-to-top-button" onClick={scrollToTop}>
+    // </button>
   );
 };
 
