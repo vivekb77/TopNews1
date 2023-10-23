d144e235fadad4860db93ff9289991a38aaf14fe
vivekbheke <vivek.bheke@stuff.co.nz>
2023-10-23 22:16:54 +1300

    auto post to twitter

--- client/netlify/functions/adsnetlify.js
+++ client/netlify/functions/adsnetlify.js
@@ -1,35 +1,48 @@
-// const { schedule } = require("@netlify/functions");
-// const axios = require('axios')
+const { schedule } = require("@netlify/functions");
+const axios = require('axios')
 
-// const handler = async function(event, context) {
-  
-// //create add
-//   await axios
-//   .post('https://topnews7.vercel.app/api/CreateAd')
-//   .then((response) => {
-//     if (response.status === 200) {
-//       console.log('Create Ad: ', response.data)
-//     }
-//   })
-//   .catch((e) => {
-//     console.error(e)
-//   })
+const handler = async function (event, context) {
 
-//   //post add
-//   await axios
-//   .post('https://topnews7.vercel.app/api/PostAd')
-//   .then((response) => {
-//     if (response.status === 200) {
-//       console.log('Post Ad: ', response.data)
-//     }
-//   })
-//   .catch((e) => {
-//     console.error(e)
-//   })
+    //create add
+    await axios
+        .post('https://topnews7.vercel.app/api/CreateAd')
+        .then((response) => {
+            if (response.status === 200) {
+                console.log('Created Ad for Twitter: ', response.data)
+            }
+        })
+        .catch((e) => {
+            console.error(e)
+        })
 
-// return {
-//     statusCode: 200,
-// };
-// };
+    //post add
+    await axios
+        .post('https://topnews7.vercel.app/api/PostAd')
+        .then((response) => {
+            if (response.status === 200) {
+                console.log('Posted Ad on Twitter: ', response.data)
+            }
+        })
+        .catch((e) => {
+            console.error(e)
+        })
 
-// exports.handler = schedule("@hourly", handler);
+    //post article url
+    await axios
+        .post('https://topnews7.vercel.app/api/PostTextTweet')
+        .then((response) => {
+            if (response.status === 200) {
+                console.log('Posted Article URL to twitter: ', response.data)
+            }
+        })
+        .catch((e) => {
+            console.error(e)
+        })
+
+    return {
+        statusCode: 200,
+    };
+
+};
+
+exports.handler = schedule("@hourly", handler);
