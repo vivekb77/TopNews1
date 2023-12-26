16ee5c7b5badb84f6cc66636322a2a801e0cd00c
vivekbheke <vivek.bheke@stuff.co.nz>
2023-12-26 19:45:20 +1300

    fixed insta post via netlify functions

--- client/netlify/functions/AutoPostInsta.js
+++ client/netlify/functions/AutoPostInsta.js
@@ -3,34 +3,63 @@ const axios = require('axios')
 
 const handler = async function (event, context) {
 
-    //create add
+    //create image for insta
     await axios
-        .post('https://topnews7.vercel.app/api/CreateAd')
+        .post('http://localhost:1337/api/CreateAd')
         .then((response) => {
             if (response.status === 200) {
-                console.log('Created Ad for Insta: ', response.data)
+                console.log('Created Image for Insta: ', response.data)
             }
         })
         .catch((e) => {
-            console.error(e)
+            console.error("Error creating image for insta " + e)
         })
 
-    //post on insta
-    await axios
-        .post('https://topnews7.vercel.app/api/PostOnInsta')
-        .then((response) => {
-            if (response.status === 200) {
-                console.log('Posted on Insta: ', response.data)
+
+    // upload image created by above to image resizer
+    try {
+        const uploadresponse = await axios.post('http://localhost:1337/api/PostOnInsta',
+            {
+                "upload_or_post_image": "uploadImage",
+                "image_name_or_id": "twitteradimage.png"
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Basic bmV3c2V4cHJlc3M6wqM1MHIwQDc9b1FYOQ==',
+                }
+            });
+            
+        if (uploadresponse.data.status === 'ok' && uploadresponse.data.image_id) {
+            console.log('Image uploaded to image resizer, Image id :', uploadresponse.data.image_id);
+
+            //post on insta
+            const postresponse = await axios.post('http://localhost:1337/api/PostOnInsta',
+                {
+                    "upload_or_post_image": "postImage",
+                    "image_name_or_id": uploadresponse.data.image_id
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Basic bmV3c2V4cHJlc3M6wqM1MHIwQDc9b1FYOQ==',
+                    }
+                });
+            if (postresponse.data.status === 'ok') {
+                console.log("Image posted to Insta");
+            }else{
+                console.log("Image not posted to Insta. Error: ", postresponse.data.message);
             }
-        })
-        .catch((e) => {
-            console.error(e)
-        })
-
-    return {
-        statusCode: 200,
-    };
+        }else{
+            console.log("Image Not uploaded to image resizer. Error: ", uploadresponse.data.message);
+        }
+    } catch (error) {
+        console.error("Error while uploading to image resizer and posting to insta " + error.message);
+    }
 
 };
 
 exports.handler = schedule("@hourly", handler);
+
+// netlify functions:serve
+// http://localhost:9999/.netlify/functions/<name>
--- server/Ads/PostOnInsta.js
+++ server/Ads/PostOnInsta.js
@@ -10,37 +10,70 @@ const imageResizerKey = "245e3a1588bbdaa173f40624ef424a38e76589f4";
 
 router.post('/PostOnInsta', async (req, res) => {
 
-    try {
-        //only post from 7am to 11pm
-        const currentTimeUTC = new Date();
-        const nztOffset = 13 * 60;
-        const currentTimeNZT = new Date(currentTimeUTC.getTime() + nztOffset * 60 * 1000);
-        const startTimeNZT = new Date(currentTimeNZT);
-        startTimeNZT.setHours(6, 55, 0);
-        const endTimeNZT = new Date(currentTimeNZT);
-        endTimeNZT.setHours(23, 5, 0);
-        if (currentTimeNZT >= startTimeNZT && currentTimeNZT <= endTimeNZT) {
-            console.log("Posting on Insta: " + currentTimeNZT);
-            await getImageUrl();
-            return res.json({ status: 'ok', message: "Posted on Insta: " + currentTimeNZT })
-        } else {
-            console.log("Skipping posting on Insta: " + currentTimeNZT);
-            return res.json({ status: 'ok', message: "Skipped posting on Insta: " + currentTimeNZT })
+    if (req.headers['authorization'] === 'Basic bmV3c2V4cHJlc3M6wqM1MHIwQDc9b1FYOQ==') {
+
+        const upload_or_post_image = req.body.upload_or_post_image;
+        const image_name_or_id = req.body.image_name_or_id;
+
+        try {
+            //only post from 7am to 11pm
+            const currentTimeUTC = new Date();
+            const nztOffset = 13 * 60;
+            const currentTimeNZT = new Date(currentTimeUTC.getTime() + nztOffset * 60 * 1000);
+            const startTimeNZT = new Date(currentTimeNZT);
+            startTimeNZT.setHours(6, 55, 0);
+            const endTimeNZT = new Date(currentTimeNZT);
+            endTimeNZT.setHours(23, 5, 0);
+            if (currentTimeNZT >= startTimeNZT && currentTimeNZT <= endTimeNZT) {
+
+                if (upload_or_post_image === "uploadImage") {
+                    console.log("Uploading image to image resizer");
+                    const image_id = await uploadImageToImageResizer(image_name_or_id);
+                    if (image_id) {
+                        return res.json({ status: `ok`, message: `Uploaded image to Image Resizer`, image_id: image_id, uploaded_at: currentTimeNZT })
+                    } else {
+                        return res.json({ status: `error`, message: `Uploaded image to Image Resizer failed`, upload_tried_at: currentTimeNZT })
+                    }
+                } else if (upload_or_post_image === "postImage") {
+                    console.log("Posting image to Insta");
+                    var currentHour = new Date().getHours();
+                    let postsuccessfail;
+                    if (currentHour % 2 === 0) {
+                        postsuccessfail = await InstaPostAPost(image_name_or_id);
+                        await ImageResizerDeleteImage(image_name_or_id);
+                    } else {
+                        postsuccessfail = await InstaPostAStory(image_name_or_id);
+                        // postsuccessfail = await PostAReel();
+                        await ImageResizerDeleteImage(image_name_or_id);
+                    }
+                    if (postsuccessfail) {
+                        return res.json({ status: `ok`, message: "Posted image to Insta", posted_at: currentTimeNZT })
+                    } else {
+                        return res.json({ status: `error`, message: "Not posted image to Insta", post_tried_at: currentTimeNZT })
+                    }
+                } else {
+                    console.log("Not uploaded or posted. Valid values are uploadImage or postImage");
+                    return res.json({ status: 'error', message: `Not uploaded or posted. Valid values are uploadImage or postImage ` + currentTimeNZT })
+                }
+            } else {
+                console.log("Skipping uploading or posting on Insta: " + currentTimeNZT);
+                return res.json({ status: 'ok', message: "Skipped posting or uploading on Insta: " + currentTimeNZT })
+            }
+        } catch (err) {
+            console.log(err);
+            return res.json({ status: 'error', message: err.message })
         }
-    } catch (err) {
-        console.log(err);
-        return res.json({ status: 'error' })
+    } else {
+        return res.json({ status: 'error', message: `Invalid user` });
     }
 })
 
-//to upload image to insta, url is needed , cant upload from local storage
-async function getImageUrl() {
-    let image_url;
-    let post_caption = `Read latest NZ NEWS, link in bio`
-    let image_id
+//to upload image to insta, url is needed , cant upload from local storage, here we upload image created by createAd.js to imageresizer
+async function uploadImageToImageResizer(image_name_or_id) {
 
     try {
-        const localImagePath = '/tmp/twitteradimage.png'; // this is created by createad.js
+        // const localImagePath = '/tmp/twitteradimage.png'; // this is created by createad.js
+        const localImagePath = `/tmp/${image_name_or_id}`; // this is created by createad.js
         const imageBuffer = fs.readFileSync(localImagePath);
 
         const imageUploadResponse = await axios.post(
@@ -50,29 +83,25 @@ async function getImageUrl() {
             }
         );
         if (imageUploadResponse.status === 200 && imageUploadResponse.data.success === true) {
-            image_id = imageUploadResponse.data.response.id
-            image_url = `https://newsexpress.imageresizer.io/${image_id}?size=1080x1800&format=webp&qualty=80`;
+            let image_id = imageUploadResponse.data.response.id
             console.log("Image uploaded to image resizer");
-            //now post insta post and story, post on even hour, story on odd hour
-            var currentHour = new Date().getHours();
-            if (currentHour % 2 === 0) {
-                await PostAPost(image_url, post_caption);
-            } else {
-                await PostAStory(image_url);
-            }
-            // await PostAReel();
-            await DeleteImage(image_id);
+            return image_id;
         } else {
             console.log("Did not upload local image to imageresizer");
+            return null;
         }
     } catch (error) {
         console.log(`Could not upload local image to imageresizer , Error is ${error.message}`);
+        return null;
     };
 
 }
 
-async function PostAPost(image_url, post_caption) {
+async function InstaPostAPost(image_name_or_id) {
     try {
+        image_url = `https://newsexpress.imageresizer.io/${image_name_or_id}?size=1080x1800&format=webp&qualty=80`;
+        let post_caption = `Read latest NZ NEWS, link in bio`;
+
         //post upload
         const response = await axios.post(
             `https://graph.facebook.com/v18.0/${instagram_business_account_id}/media?image_url=${image_url}&caption=${post_caption}&access_token=${longLivedAccessToken}`,
@@ -87,13 +116,17 @@ async function PostAPost(image_url, post_caption) {
                 console.log('Insta Post posted')
             }
         }
-
+        return "success";
     } catch (error) {
         console.error('Error posting Insta post:', error.response ? error.response.data : error.message);
+        return null;
     }
 }
 
-async function PostAStory(image_url) {
+async function InstaPostAStory(image_name_or_id) {
+    image_url = `https://newsexpress.imageresizer.io/${image_name_or_id}?size=1080x1800&format=webp&qualty=80`;
+    let post_caption = `Read latest NZ NEWS, link in bio`;
+
     try {
         //story upload
         const response = await axios.post(
@@ -110,14 +143,15 @@ async function PostAStory(image_url) {
                 console.log('Insta Story posted')
             }
         }
-
+        return "success";
     } catch (error) {
         console.error('Error posting Insta Story:', error.response ? error.response.data : error.message);
+        return null;
     }
 }
 
 
-async function DeleteImage(image_id) {
+async function ImageResizerDeleteImage(image_id) {
     try {
         const deletedImage = await axios.get(
             `https://api.imageresizer.io/v1/images/${image_id}/delete?key=${imageResizerKey}`,
@@ -157,8 +191,8 @@ async function PostAReel() {
 
 module.exports = router;
 
- // await getLongLivedAccessToken(); //only use this to generate long lived token
- async function getLongLivedAccessToken() {
+// await getLongLivedAccessToken(); //only use this to generate long lived token
+async function getLongLivedAccessToken() {
     //get long live token from short live token
     // the token from facebook graph explorer is short lived ie 1 hour, so we need to create long lived 60 days token from short live token
     //https://graph.facebook.com/{graph-api-version}/oauth/access_token?grant_type=fb_exchange_token&client_id={app-id}&client_secret={app-secret}&fb_exchange_token={your-access-token}
